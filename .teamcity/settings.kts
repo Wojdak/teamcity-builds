4fe04a20246f889effd7b3d4b89c15b9d9e13986
Super user <Super user@TeamCity>
2024-02-12 22:04:51 +0000

    TeamCity change in 'example-workflow' project: build configuration was created from URL

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,57 +1,60 @@
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
 
 version = "2023.11"
 
-val infraRepo = GitVcsRoot {
-    id("Infra_Repo")
-    name = "Infrastructure Repository"
-    url = "https://github.com/Wojdak/workflow-example"
-    branch = "main"
-}
-
 project {
-    buildType<InfraBuild>()
+
+    vcsRoot(HttpsGithubComWojdakWorkflowExampleRefsHeadsMain)
+
+    buildType(ExampleWorkflow)
 }
 
-class InfraBuild : BuildType({
-    id("InfraBuild_Build")
-    name = "Terraform and Ansible Build"
-    description = "Build configuration for deploying infrastructure with Terraform and Ansible"
+object ExampleWorkflow : BuildType({
+    name = "example-workflow"
 
     vcs {
-        root(infraRepo)
+        root(HttpsGithubComWojdakWorkflowExampleRefsHeadsMain)
     }
 
-    steps {
-        script {
-            name = "Execute Terraform Commands"
-            scriptContent = """
-                cd terraform
-                terraform init
-                terraform plan -out=tfplan
-                terraform apply -auto-approve tfplan
-                terraform output droplet_ip > ../droplet_ip.txt
-            """.trimIndent()
-        }
-
-        script {
-            name = "Execute Ansible Playbook"
-            scriptContent = """
-                droplet_ip=$(cat ../droplet_ip.txt)
-                cd ../ansible
-                echo "[droplet]" > inventory.ini
-                echo "\$droplet_ip" >> inventory.ini
-                ansible-playbook -i inventory.ini deploy.yml --private-key=~/.ssh/id_rsa 
-            """.trimIndent()
-        }
-
-        script {
-            name = "Execute Validation Script"
-            scriptContent = """
-                droplet_ip=$(cat ../droplet_ip.txt)
-                cd ..
-                ./validate.sh "\$droplet_ip"
-            """.trimIndent()
+    triggers {
+        vcs {
         }
     }
+
+    features {
+        perfmon {
+        }
+    }
+})
+
+object HttpsGithubComWojdakWorkflowExampleRefsHeadsMain : GitVcsRoot({
+    name = "https://github.com/Wojdak/workflow-example#refs/heads/main"
+    url = "https://github.com/Wojdak/workflow-example"
+    branch = "refs/heads/main"
+    branchSpec = "refs/heads/*"
 })

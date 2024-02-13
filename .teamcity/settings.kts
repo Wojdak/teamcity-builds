d81c4e2994da65e5be4a46487d24739846ad9ac6
Super user <Super user@TeamCity>
2024-02-13 15:53:29 +0000

    TeamCity change in 'test-project' project: build configuration was created from URL

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,8 @@
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -26,14 +29,48 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2023.11"
 
 project {
-  buildType(HelloWorld)
+
+    vcsRoot(HttpsGithubComWojdakSampleAppRefsHeadsMain)
+
+    buildType(BuildFlaskApp)
+    buildType(HelloWorld)
 }
 
-object HelloWorld: BuildType({
+object BuildFlaskApp : BuildType({
+    name = "Build Flask App"
+
+    vcs {
+        root(HttpsGithubComWojdakSampleAppRefsHeadsMain)
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+
+    features {
+        perfmon {
+        }
+    }
+})
+
+object HelloWorld : BuildType({
     name = "Hello world"
+
     steps {
         script {
             scriptContent = "echo 'Hello world!'"
         }
+    }
+})
+
+object HttpsGithubComWojdakSampleAppRefsHeadsMain : GitVcsRoot({
+    name = "https://github.com/Wojdak/sample-app#refs/heads/main"
+    url = "https://github.com/Wojdak/sample-app"
+    branch = "refs/heads/main"
+    branchSpec = "refs/heads/*"
+    authMethod = password {
+        userName = "Wojdak"
+        password = "credentialsJSON:fa36a0d7-2e4f-4ffd-a808-e1abeaa7dc90"
     }
 })

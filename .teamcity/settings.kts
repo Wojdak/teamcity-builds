5099811528876e63f004ae2fc50538bd80c8319f
Super user <Super user@TeamCity>
2024-02-13 15:58:29 +0000

    TeamCity change in 'test-project' project: 2 build step(s) were added to 'Build' build configuration

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.dockerCommand
+import jetbrains.buildServer.configs.kotlin.buildSteps.python
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
@@ -42,6 +44,22 @@ object Build : BuildType({
 
     vcs {
         root(HttpsGithubComWojdakSampleAppGitRefsHeadsMain)
+    }
+
+    steps {
+        dockerCommand {
+            id = "DockerCommand"
+            commandType = build {
+                source = file {
+                    path = "Dockerfile"
+                }
+            }
+        }
+        python {
+            id = "python_runner"
+            command = pytest {
+            }
+        }
     }
 
     triggers {
